eab3b64484565150c4f47049a5465afa0fc7552f
metaltroop <kalepiyush02@gmail.com>
2025-06-03 01:03:18 +0530

    deptActivityRoutes

--- citysynergy/routes/deptActivityRoutes.js
+++ citysynergy/routes/deptActivityRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const deptActivityController = require('../controllers/DeptActivityController');
+const deptActivityController = require('../controllers/deptActivityController');
 const authenticateUser = require('../middleware/authMiddleware');
 const authMiddleware = require('../middleware/authMiddleware');
 
